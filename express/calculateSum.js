8248f5e4f7b8ae3df4361c2d5096adf2242c61ad
Parth <kanhamahajan73@gmail.com>
2025-09-10 21:27:25 +0530

    rewite the code

--- express/calculateSum.js
+++ express/calculateSum.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
 
-const port = 3500;
 function calcSum(n) {
-
-    let ans = 0; 
-    for (let i = 1; i <= n; i++) {
-        ans += i;
+    let sum = 0;
+    for (let i = 0; i <= n; i++) {
+        sum += i;
+        
     }
-    return ans;
+    return sum;
 }
-
-app.get('/', function(req, res) {
+app.get('/health-checkup', function(req, res) {
     const n = req.query.n;
-    const ans= calcSum(n);
-    res.send(ans);
-});
-app.listen(port);   
+    const ans = calcSum(n);
+    res.send(`the final sum is : ${ans}`);
+})
+
+app.listen(3000);
