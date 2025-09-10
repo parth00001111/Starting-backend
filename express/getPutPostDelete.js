f94b2b9c2cc79db70cfa5260d50fb126dfc01c2e
Parth <kanhamahajan73@gmail.com>
2025-09-11 01:47:08 +0530

    revised postman request 1

--- express/getPutPostDelete.js
+++ express/getPutPostDelete.js
@@ -1,60 +1,37 @@
-const express = require('express');
-const app = express();
+const express = require('express'); 
+const app = express(); 
 const users = [{
     name: "parth",
     kidneys: [{
-        healthy: false 
+        healthy: false
     }]
 }];
-app.use(express.json());
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     const parthKidneys = users[0].kidneys;
     const numberOfKidneys = parthKidneys.length;
-    let numberOfHealthyKidneys = 0;
+    let numberofHealthyKidneys = 0;
+    
     for (let i = 0; i < parthKidneys.length; i++) {
         if(parthKidneys[i].healthy) {
-            numberOfHealthyKidneys++;
+            numberofHealthyKidneys++;
         }
     }
-    const numberOfUnHealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
-
+    let numberofUnHealthyKidneys = numberOfKidneys - numberofHealthyKidneys;
     res.json({
         numberOfKidneys,
-        numberOfHealthyKidneys,
-        numberOfUnHealthyKidneys
-    });
+        numberofHealthyKidneys,
+        numberofUnHealthyKidneys
+    })
 });
-app.post('/', function(req, res) {
+app.use(express.json());
+
+app.post('/', function (req, res) {
     const isHealthy = req.body.isHealthy;
     users[0].kidneys.push({
         healthy: isHealthy
-})
-res.json({
-    msg: "Done"
-})
-})
-
-app.put('/', function(req, res) {
-    for (let i = 0; i < users[0].kidneys.length; i++) {
-        users[0].kidneys[i].healthy = true;
-    }
+    })
     res.json({
         msg: "done"
     })
-})
- 
-app.delete('/', function(req, res) {
-    const newKidneys = [];
-    for (let i = 0; i < users[0].kidneys.length; i++) {
-        if(users[0].kidneys[i].healthy) {
-            newKidneys.push({
-                healthy: true
-            })
-        }
-    }
-    users[0].kidneys = newKidneys;
-    res.json({
-        msg: "done"
-    })
-})
+});
 app.listen(3000);
