e2468cd1601094b2ff9510854c546837883f20a9
Parth <kanhamahajan73@gmail.com>
2025-09-16 23:34:51 +0530

    revised the code with comments

--- middleware/equi.js
+++ middleware/equi.js
@@ -1,30 +1,59 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// app.get("/health-checkup", function(req, res) {
+//     const username = req.headers.username;// in headers because hidden data hona chaiye username and password
+
+//     const password= req.headers.password; in headers because hidden data hona chaiye username and password
+
+//     const kidneyId = req.query.kidneyId;
+
+//     if(username != "parth" || password != "pass") {//if (!(name == "parth" && passward == "pass"))//dono same hai
+
+//         res.status(400).json({
+//             "msg": "somgthing is wrong with your inputs"
+            
+//         });
+//         return;
+//     }
+
+//     if (kidneyId != 1 && kidneyId != 2) {
+//         res.status(400).json({
+//             "msg": "something is wrong with your input"
+//         });
+//         return;
+//     }
+
+//     res.json({
+//         msg: "your kidney is fine"
+//     })
+// })
+
+const express = require ('express');
 const app = express();
-app.get("/health-checkup", function(req, res) {
+
+app.get('/', function(req, res) {
     const username = req.headers.username;
-
-    const password= req.headers.password;
-
+    const password = req.headers.password;
     const kidneyId = req.query.kidneyId;
 
-    if(username != "parth" || password != "pass") {
+    if (!(username == "parth" && password == "pass")) {
         res.status(400).json({
-            "msg": "somgthing is wrong with your inputs"
-            
+            msg: "you input is invalid"
         });
         return;
     }
 
-    if (kidneyId != 1 && kidneyId != 2) {
+    if(!kidneyId == 1 && !kidneyId == 2) {
         res.status(400).json({
-            "msg": "something is wrong with your input"
+            msg: "somethig is wrong with you inputs"
         });
         return;
     }
-
     res.json({
-        msg: "your kidney is fine"
-    })
-})
+        msg: "everything is fine"
+    });
+});
 
-// app.listen(3000);
+
+
+app.listen(3000);
